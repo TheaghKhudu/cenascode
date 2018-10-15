0a14e76343d3a6184095489fc23f2410d4a85ef9
TheaghKhudu <43776982+TheaghKhudu@users.noreply.github.com>
2018-10-15 22:08:47 +0400

    Add files via upload

--- TestMaker/expression_maker.cpp
+++ TestMaker/expression_maker.cpp
@@ -1,9 +1,20 @@
-/* /
+/* /14.10.2018
 This program generates mathematical expressions which are supposed to test our main calculator program.
 It can generate expressions with 3 different difficulty levels: easy, normal, hard.
 The working principle of generating such expressions relies on P.O.C.L.(Probabilistic Operation Container List)
 Therefore it is easier to change the structure of expressions in any time just by changing that list.
 Easily managable, safe codes containing static lists and 'generic' functions to be able to work with those lists.
+
+V2 /15.10.2018
+Upgraded:
+    -Precise P.O.C.L. structure
+    -factorial order bug fixed (5!+3)
+    -opened parantheses bug fixed
+    -3 difficulty levels have been set up
+    -first position operators have been organized
+    -fail counter has been added in order to increase efficiency
+    -function reducement
+    -new mode("silent") has been added
 */
 
 #include<iostream>
@@ -11,6 +22,7 @@ Easily managable, safe codes containing static lists and 'generic' functions to 
 #include<cstring>
 #include<cstdlib>
 #include<ctime>
+#include<unistd.h>
 #include"../User_Interface/exception.hpp"
 
 #define MAX_NB_OPR 24
@@ -40,80 +52,90 @@ typedef struct OPERATION{
 typedef struct POCL{
     private:
     Operation operation;
-    unsigned prob[MAX_NB_OPR-1];
-    int PROB[MAX_NB_OPR][MAX_NB_RORD];
+    unsigned* prob;
+    int PROB[MAX_NB_OPR-1][MAX_NB_RORD];
     int OFFSET[MAX_NB_OPR-1];
     string expression;
 
     public:
-    POCL(const Operation& operation): operation(operation) {}
-    void set_prob(const unsigned* prob);
+    POCL(const Operation& operation,unsigned* prob): 
+        operation(operation) , prob(prob) {}
     void init_PROB();
-    unsigned get_rand_opr_index(unsigned opr_index);
-    string get_expression(unsigned max_length);
+    unsigned get_rand_opr_index(unsigned opr_index, bool is_fail);
+    string get_expression(unsigned max_length, unsigned level);
 
     void print_prob();
     void print_PROB();
 }POCL;
 
-//Expression_Lit POCL::list=
-
 int main(int argc, char* argv[]){
 //the syntax : ./expr_maker [-numerical] [-level] [char length]
     
-    if(argc!=4){ cout<<"The syntax : ./expr_maker [-numerical] [-level] [char length]\n"; exit(0); }
+    if(!(argc==4 || argc==5)){ cout<<"The syntax : ./expr_maker [-numerical] [level] [char length]\n"; exit(0); }
+    bool silent=false;
+    if(argc==5 && strcmp(argv[4],"-s")==0){ silent=true; }
     bool numerical=strcmp(argv[1],"-n")==0? NUMERICAL : N_NUMERICAL;
     int args[2]={atoi(argv[2]),atoi(argv[3])};
-    if(0>args[1] || args[1]>500){ cout<<"char length should be less than 500!\n"; exit(0); }
+    if(args[1]<=0 || args[1]>500){ cout<<"char length should be less than 500!\n"; exit(0); }
     string expression;
 
     const Operation operation;
-    POCL pocl(operation);
-    unsigned* prob=nullptr;
-    
+    const unsigned* prob=nullptr;
+
     //set your lists below by changing the numbers of correspoinding prob
-    const unsigned prob_EASY[23]{0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,39,65,91,156,221,325,429,507};
-    const unsigned prob_NORMAL[23]{10,20,23,24,25,26,27,28,29,30,31,32,33,34,35,40,45,50,51,70,80,81,82};
-    const unsigned prob_HARD[23]{20,40,43,53,63,73,83,93,93,93,93,93,93,93,93,96,97,98,99,110,140,141,142};
+    // 2.      0%
+    // 13.     ~0.365%
+    // 3.      ~9.489%
+    // 2.      ~23.722%
+    // 2.      ~37.956%
+    // 1.      ~28.467%
+    const unsigned prob_EASY[MAX_NB_OPR-1]{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,26,26,26,65,65,104,104,78};
+    const unsigned prob_NORMAL[MAX_NB_OPR-1]{15,500,2,2,2,2,2,2,2,2,2,2,2,2,2,150,150,150,120,120,80,80,150};
+    const unsigned prob_HARD[MAX_NB_OPR-1]{100,50,10,10,10,10,10,10,10,10,10,10,10,10,10,130,65,65,65,65,65,65,30};
 
     switch(args[0]){
         case EASY:
-            prob=const_cast<unsigned*>(prob_EASY);
+            prob=prob_EASY;
             break;
         case NORMAL:
-            prob=const_cast<unsigned*>(prob_NORMAL);
+            prob=prob_NORMAL;
             break;
         case HARD:
-            prob=const_cast<unsigned*>(prob_HARD);
+            prob=prob_HARD;
             break;
         default:
             cout<<"Difficulty level can be 0/1/2!\n";
             exit(0);
     }
     srand(time(nullptr));
-    //ofstream out; unsigned counter=0;
-    //out.open(OUT_FILE);
-    //GET:
-    //++counter;
-    if(numerical){
-        //cout<<"Init prob : "<<'\n';
-        pocl.set_prob(prob);
-        //pocl.print_prob();
-        //cout<<"Init PROB : "<<'\n';
-        pocl.init_PROB();
-        //pocl.print_PROB();
-        //cout<<"Get Expression : "<<'\n';
-        expression=pocl.get_expression(args[1]);
-    } else{
-        cout<<"Not ready to generate!\n";
-        exit(0);
-    }
 
+    POCL pocl(operation, const_cast<unsigned*>(prob));
+    string file_name; int NB, opt;
+    cout<<"Enter a file name(Press : and hit enter for default name) : ";
+    cin>>file_name;
+    if(file_name.compare(":")==0){ file_name=OUT_FILE; }
+    cout<<"Enter the number of expressions to be generated : ";
+    cin>>NB;
     ofstream out;
-    out.open(OUT_FILE);
-    out<<expression;
-    expression.clear();
-    //if(counter<50000){ goto GET; }
+    out.open(file_name);
+    // if(!out.good() || !out.eof()){
+    //     cout<<"File isn't empty. (1)Rewrite or (2)Append? ";
+    //     cin>>opt;
+    //     if(opt==2){ out.seekp(0,ios::end); }
+    // }
+    for(int nb=0;nb<NB;++nb){
+        if(numerical){
+            pocl.init_PROB();
+            expression=pocl.get_expression(args[1],args[0]);
+        } else{
+            cout<<"Not ready to generate!\n";
+            out.close();
+            exit(0);
+        }
+        out<<expression<<'\n';
+        if(!silent){ cout<<"Expression "<<nb+1<<" : "<<expression<<'\n'; }
+        expression.clear();
+    }
     out.close();
 
     return 0;
@@ -148,7 +170,7 @@ const unsigned Operation::order[MAX_NB_OPR][MAX_NB_RORD]={
     {0, 22, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M},
     {0, 22, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M},
     {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 22, M, M},
-    {1, 16, 17, 18, 19, 20, 21, 22, M, M, M, M, M, M, M, M, M},
+    {1, 16, 17, 18, 19, 20, 21, M, M, M, M, M, M, M, M, M, M},
     {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 22, M, M},
     {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 22, M, M},
     {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 22, M, M},
@@ -168,12 +190,8 @@ unsigned Operation::get_order(unsigned i, unsigned j){ return Operation::order[i
 
 /* P.O.C.L. block */
 
-void POCL::set_prob(const unsigned* prob){
-    for(unsigned i=0;i<23;++i){ this->prob[i]=prob[i]; }
-}
-
 void POCL::init_PROB(){
-    unsigned ord_index=0;
+    unsigned ord_index=0, tmp=0;
     for(unsigned i=0;i<MAX_NB_OPR-1;++i){
         PROB[i][0]=0;
         for(unsigned j=1;j<=MAX_NB_RORD;++j){
@@ -182,80 +200,104 @@ void POCL::init_PROB(){
                 PROB[i][j]=-1;    
                 break;
             }
-            PROB[i][j]=prob[ord_index];
-        }
+            for(unsigned k=0;k<1;++k){ tmp+=prob[ord_index]; }
+            PROB[i][j]=tmp;
+        }   tmp=0;
     }
 }
 
-unsigned POCL::get_rand_opr_index(unsigned opr_index){
-    //srand(time(nullptr));
-    //cout<<"offset["<<opr_index<<"] : "<<OFFSET[opr_index]<<'\n';
+unsigned POCL::get_rand_opr_index(unsigned opr_index, bool is_fail){
+    static unsigned fail_counter=0;
+    if(is_fail){ ++fail_counter; }
+    if(fail_counter==3){ fail_counter=0; return 1; }
     int r=rand()%OFFSET[opr_index];
-    for(unsigned i=0;i<22;++i){ //i - index
-        //cout<<"Comparing : "<<PROB[opr_index][i]<<"<="<<r<<"<"<<PROB[opr_index][i+1]<<'\n';
+    for(unsigned i=0;i<22;++i){
         if(PROB[opr_index][i]<=r && r<PROB[opr_index][i+1]){
-            //cout<<"Returned new opr_index : "<<i<<'\n';
             return i;
         }
     }
 }
 
-string POCL::get_expression(unsigned max_length){
-    //srand(time(nullptr));
+string POCL::get_expression(unsigned max_length, unsigned level){
     unsigned index=22, size=0;
-    for(unsigned i=0;i<23;++i){
+    for(unsigned i=0;i<MAX_NB_OPR-1;++i){
         OFFSET[i]=-1;
         for(unsigned j=0;j<17;++j){
             if(PROB[i][j]==-1){ break; }
             OFFSET[i]=PROB[i][j];
         }
     }
-    if(!expression.empty()){ expression.clear(); }
-    // cout<<"OFFSET : \n";
-    // for(unsigned i=0;i<23;++i){
-    //     if(OFFSET[i]==0){ cout<<"offset is 0 :(\n"; continue; }
-    //     cout<<OFFSET[i]<<" ";
-    // }   cout<<'\n';
+    if(!expression.empty()){ expression.clear(); }    
+    unsigned beg; int op=0;
+    switch(level){
+        case 0:{
+            beg=rand()%3;
+            if(beg==0){ expression+="-"; size=1;  index=21; }
+            else if(beg==1){ expression+="+"; size=1; index=20; }
+            else if(beg==2){ expression+=to_string(rand()%9+1); size=1; }
+        }   break;
+        case 1:{
+            beg=rand()%12;
+            if(beg<2){ expression+="-"; size=1; index=21; }
+            else if(beg<4){ expression+="+"; size=1; index=20; }
+            else if(beg<6){ expression+=to_string(rand()%9+1); size=1; index=22; }
+            else if(beg<12){
+                beg=rand()%14;
+                if(beg==1){ beg=14; }
+                else if(beg==0){ ++op; }
+                expression+=Operation::get_name(beg);
+                size=(beg==0)? 1 : (beg==2? 3 : 6 );
+                index=beg;
+            }
+        }   break;
+        case 2:{
+            beg=rand()%17;
+            if(beg<2){ expression+="-"; size=1; index=21; }
+            else if(beg<4){ expression+="+"; size=1; index=20; }
+            else if(beg<5){ expression+=to_string(rand()%9+1); size=1; index=22; }
+            else if(beg<17){
+                beg=rand()%14;
+                if(beg==1){ beg=14; }
+                else if(beg==0){ ++op; }
+                expression+=Operation::get_name(beg);
+                size=(beg==0)? 1 : (beg==2? 3 : 6 );
+                index=beg;
+            }
+        }
+    }
     
-    unsigned beg=rand()%3;
-    if(beg==0){ expression+="-"; size=1;  index=21; }
-    else if(beg==1){ expression+="+"; size=1; index=20; }
-    else if(beg==2){ expression+=to_string(rand()%9+1); size=1; }
-    unsigned index1=0, i=0; int op=0;
+    unsigned index1=0, i=0; bool is_fail=false;
     while(size<max_length){
-        index1=get_rand_opr_index(index);
+        index1=get_rand_opr_index(index,is_fail);
         i=Operation::get_order(index,index1);
-        if(i==1 && op==0){ continue; }
+        if(i==1 && op==0){ is_fail=true; continue; }
+        is_fail=false;
         index=i;
-        //cout<<"index + index1 : "<<index<<" and "<<index1<<'\n';
-        if(index==22){ /*cout<<"index=22!\n";*/ i=rand()%9+1; expression+=to_string(i); }
+        if(index==22){ i=rand()%9+1; expression+=to_string(i); }
         else{
             if(index==0){ ++op; }
-            else if(index==1 && op>0){ --op; }
+            else if(index==1){ --op; }
             expression+=Operation::get_name(index);
         }
         size+=Operation::jump_length(index);
-        //cout<<"\t\texpression : "<<expression<<'\n';
     }
     size=expression.size();
     if(expression[size-1]!=')' && expression[size-1]!='!' && !isdigit(expression[size-1])){
         expression+=to_string((rand()%9+1));
         if(expression[size-1]=='('){ expression+=")"; --op; }
     }
-
     for(int i=0;i<op;++i){ expression+=")"; }
-    //cout<<"Expression : "<<expression<<'\n';
     return expression;
 }
 
 void POCL::print_prob(){
-    for(unsigned i=0;i<23;++i){
+    for(unsigned i=0;i<MAX_NB_OPR-1;++i){
         cout<<this->prob[i]<<'\n';
     }
 }
 
 void POCL::print_PROB(){
-    for(unsigned i=0;i<23;++i){
+    for(unsigned i=0;i<MAX_NB_OPR-1;++i){
         for(unsigned j=0;j<17;++j){
             if(this->PROB[i][j]==-1){ break; }
             cout<<this->PROB[i][j]<<" ";
